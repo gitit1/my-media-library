0756f45bc01250d06bcee44fbf7a7ac87d37f097
git <gitit1@msn.com>
2025-03-16 13:23:36 +0200

    chore: set up environment variables and update docs

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // makes the config available everywhere
+    }),
+  ],
   controllers: [],
   providers: [],
 })
