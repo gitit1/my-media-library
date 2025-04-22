c801c21a0d6bff75ea5365fee6048afa8c21ce70
git <gitit1@msn.com>
2025-04-22 10:56:36 +0300

    feat(ui): create Manage Paths page at /settings/paths. Closes #80

--- client/src/app/pages/settings/page.tsx
+++ client/src/app/pages/settings/page.tsx
@@ -1,8 +1,18 @@
-export default function SettingsPage() {
-  return (
-    <div>
-      <h1>Settings</h1>
-      <p>Manage your general application settings.</p>
-    </div>
-  );
+'use client';
+
+import { TypographyType } from '@types';
+import { Container, Typography } from '@ui';
+import React from 'react';
+
+export default function ManagePathsPage() {
+	return (
+		<Container className="p-6">
+			<Typography type={TypographyType.H1} className="mb-4">
+				Manage File Paths
+			</Typography>
+			<Typography type={TypographyType.P}>
+				This is where you’ll manage your media folders.
+			</Typography>
+		</Container>
+	);
 }
